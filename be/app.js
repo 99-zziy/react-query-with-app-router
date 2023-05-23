f1c255a11a6e42bcfb0bfe1d5052544fe30661f5
99-zziy <wldud8229@gmail.com>
2023-05-23 15:15:14 +0900

    feat: test api 생성

--- be/app.js
+++ be/app.js
@@ -5,3 +5,8 @@ const port = 4000;
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+app.get("/test", (req, res) => {
+  console.log("Hello World!");
+  res.status(200).json({ data: "hello world" });
+});
